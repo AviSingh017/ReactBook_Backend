b48cd1f086471967cd1675c4747a6e6e9679dc2d
Avishek Singh <115368094+AviSingh017@users.noreply.github.com>
2023-07-06 12:50:57 +0530

    ReactBook Backend Deploy

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const {connection} = require("./config/db");
+const { bookRoute } = require("./routes/book.routes");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send("Hello world");
+});
+
+app.use("/", bookRoute);
+
+app.listen(process.env.port, async()=>{
+    try{
+        await connection;
+        console.log("Connected to DB");
+    }
+    catch(err){
+        console.log(err);
+    }
+    console.log(`Server running on port ${process.env.port}`);
+});
